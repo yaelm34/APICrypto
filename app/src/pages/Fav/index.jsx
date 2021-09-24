a03959ea8d2562e138dda4e3b0c507c11a03ed25
Tanjoui <tanguycad@gmail.com>
2021-09-24 15:35:15 +0200

    Ajout du routeur

---
+++ app/src/pages/Fav/index.jsx
@@ -0,0 +1,9 @@
+function Fav() {
+    return (
+        <div>
+            <h1>Mes cryptos favorites</h1>
+        </div>
+    )
+}
+
+export default Fav
---
+++ app/src/pages/List/index.jsx
@@ -0,0 +1,9 @@
+function List() {
+    return (
+        <div>
+            <h1>Ici se trouve la liste des cryptos </h1>
+        </div>
+    )
+}
+
+export default List
