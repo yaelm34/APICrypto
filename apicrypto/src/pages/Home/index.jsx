5da6396be869d739ff3495cbc753a5a4f3551a93
Yael <yaelmisseri@gmail.com>
2021-09-24 15:20:18 +0200

    architecture

--- apicrypto/src/pages/Home/index.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <div>
-            <h1>Bienvenue sur l'accueil de Cryporacle</h1>
-        </div>
-    )
-}
-
-export default Home
--- apicrypto/src/pages/List/index.jsx
+++
@@ -1,9 +0,0 @@
-function List() {
-    return (
-        <div>
-            <h1>Ici se trouve la liste des cryptos </h1>
-        </div>
-    )
-}
-
-export default List
