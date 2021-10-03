2b534327f54db50fed1e58155b55dd83bf387df5
Tanjoui <tanguycad@gmail.com>
2021-10-03 15:45:40 +0200

    improvements on favorites

--- app/src/pages/Fav/index.jsx
+++ app/src/pages/Fav/index.jsx
@@ -1,4 +1,27 @@
 
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import withToggle from "./withToggle";
+
+class Favorite extends React.Component {
+  render() {
+    const { children, isAdded, color } = this.props;
+    return (
+      <div>
+        <FontAwesomeIcon
+          icon={faHeart}
+          color={isAdded ? color : ""}
+          style={{ cursor: "pointer" }}
+        />
+        {children}
+      </div>
+    );
+  }
+}
+
+export default withToggle({ propertyName: "isAdded" })(Favorite);
+
 class Catalogue extends Component {
     constructor(props) {
     super(props);
