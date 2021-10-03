3214caf5ec7bb27752f3a9b704d71cb3f9f85e9a
Tanjoui <tanguycad@gmail.com>
2021-10-03 15:49:04 +0200

    version without favorites

--- app/src/pages/Fav/index.jsx
+++ app/src/pages/Fav/index.jsx
@@ -1,9 +1,10 @@
 //favorites page
+//is a favorite page
 
 function Fav() {
     return (
         <div>
-            <h1>Mes cryptos favorites</h1>
+            <h1>Mes cryptos favorites sont mes cryptos favorites</h1>
         </div>
     )
 }
