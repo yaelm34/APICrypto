bb28b8cab177dbab0fec642c5c8b318fd2ecd98a
Tanjoui <tanguycad@gmail.com>
2021-10-03 15:43:19 +0200

    first version of favorites

--- app/src/pages/Fav/index.jsx
+++ app/src/pages/Fav/index.jsx
@@ -1,3 +1,32 @@
+
+class Catalogue extends Component {
+    constructor(props) {
+    super(props);
+    let favoriteTopicsData = [];
+    favoriteTopicsData.push(
+        this.props.topicNames						
+            )
+    this.state = {
+        favoriteTopicsData: favoriteTopicsData
+    };
+}
+    componentWillReceiveProps(nextProps){
+    // console.log("nextProps in myTopics",nextProps);
+    let favoriteTopicsData = [];
+    favoriteTopicsData.push(
+                this.props.topicNames
+                )
+    this.setState({favoriteTopicsData});
+}
+    render() {
+    return (
+        <View key={"MyTopics"}>
+            <GridView gridViewData={this.state.favoriteTopicsData} openSubCat={this._openSubCat} parentKey={"MyTopics"} />
+        </View>
+    );
+}
+}
+
 function Fav() {
     return (
         <div>
